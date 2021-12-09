00ff955afc6ff40d93815a26d91c013e5f4b44f3
George Joseph <thegeorgejoseph@gmail.com>
2021-12-09 12:25:17 -0800

    stringGeneration now adheres to size constraints

--- A_G_S/stringGenerator.py
+++ A_G_S/stringGenerator.py
@@ -5,23 +5,42 @@ def stringGenerator():
     if len(sys.argv) >1:
         input_filename=sys.argv[1]
     results = []
+    idxCounts = []
+    initialStrings = []
     flag = False
+    validate = False
     with open(input_filename) as file:
         for line in file:
             line = line.strip('\n')
             if line.isdigit() is False:
                 if flag is True:
                     results.append(string)
+                    idxCounts.append(idxCount)   
                 string = line
                 flag = False
+                idxCount = 0
+                initialStrings.append(string)
             else:
                 flag = True
                 result = string[:int(line)+1] + string + string[int(line)+1:]
                 string = result
+                idxCount += 1
                 # print(string)
                 
         if flag is True:
             results.append(string)
+            idxCounts.append(idxCount)
             
     file.close()
+    if len(results[0]) > 2 ** idxCounts[0] * len(initialStrings[0]):
+        results[0] = results[0][:(2**idxCounts[0]*len(initialStrings[0]))]
+        validate = True
+    if len(results[1]) > 2 ** idxCounts[1] * len(initialStrings[1]):
+        results[1] = results[1][:(2**idxCounts[1]*len(initialStrings[1]))]
+        validate = True
+    if validate: print("The strings have been validated to meet the size constraint")
     return results[0],results[1]
+
+
+if __name__ == "__main__":
+    stringGenerator()
