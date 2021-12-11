d4245658beab0a747ae57000483ef741f19ef3ec
srinicoder035 <k.s.srinivasan351998@gmail.com>
2021-12-10 22:09:51 -0800

    Completion of the Project

--- 6493967427_8486068836_8234178058/6493967427_8486068836_8234178058_efficient.py
+++ 6493967427_8486068836_8234178058/6493967427_8486068836_8234178058_efficient.py
@@ -1,6 +1,5 @@
 from stringGenerator import stringGenerator
 from utils import get_memory_diff, get_memory_point, get_time_diff, get_time_point, write_output
-from A_G_S_basic import dynamicSequentialAlignmentBasic
 
 mapping = {"A":0,"C":1,"G":2,"T":3}
 penalty = [
@@ -11,6 +10,109 @@ penalty = [
 ]
 delta = 30
 
+
+#Basic Version
+def dynamicSequentialAlignmentBasic(string1,string2):
+    start_time = get_time_point()
+    start_memory = get_memory_point()
+
+    m,n = len(string1),len(string2)
+    dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+    # print(dp)
+    
+    for i in range(n+1):
+        dp[0][i] = i * delta
+    for i in range(m + 1):
+        dp[i][0] = i * delta 
+        
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            if string1[i-1] == string2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                index1,index2 = mapping[string1[i-1]],mapping[string2[j-1]]
+                currentPen = penalty[index1][index2]
+                dp[i][j] = min(dp[i-1][j-1] + currentPen, dp[i-1][j] + delta, dp[i][j-1] + delta)
+            
+    max_len = m + n 
+    i = m
+    j = n
+    xIdx, yIdx = max_len, max_len
+    string1result = [None for _ in range(max_len+1)]
+    string2result = [None for _ in range(max_len+1)]
+    
+    while ( i!=0 and j!=0 ):
+        if string1[i-1] == string2[j-1]:
+            string1result[xIdx] = string1[i-1]
+            xIdx -= 1
+            i -= 1
+            string2result[yIdx] = string2[j-1]
+            yIdx -= 1
+            j -= 1
+            
+            
+        elif dp[i-1][j-1] + penalty[mapping[string1[i-1]]][mapping[string2[j-1]]] == dp[i][j]:
+            string1result[xIdx] = string1[i-1]
+            string2result[yIdx] = string2[j-1]
+            xIdx -= 1
+            yIdx -= 1
+            i -= 1
+            j -= 1
+            
+        elif dp[i-1][j] + delta == dp[i][j]:
+            string1result[xIdx] = string1[i-1]
+            string2result[yIdx] = '_'
+            xIdx -= 1
+            yIdx -= 1
+            i -= 1
+            
+        elif dp[i][j-1] + delta == dp[i][j]:
+            string1result[xIdx] = '_'
+            string2result[yIdx] = string2[j-1]
+            xIdx -= 1
+            yIdx -= 1
+            j -= 1
+            
+        # else:
+        #     print(i,j,dp[i][j])
+        #     print(string1[29],string2[28])
+        #     print(dp[29-1][28-1] + penalty[mapping[string1[29-1]]][mapping[string2[28-1]]])
+        #     print(dp[29-1][28] + delta)
+        #     dp[29][28-1] + delta
+        #     print("exception")
+    
+    # print("step1 done")
+    while xIdx > 0 :
+        if i > 0:
+            i -= 1
+            string1result[xIdx] = string1[i]
+            xIdx -= 1
+        else:
+            string1result[xIdx] = '_'
+            xIdx -= 1
+        
+    while yIdx > 0:
+        if j > 0:
+            j -= 1
+            string2result[yIdx] = string2[j]
+            yIdx -= 1
+        else:
+            string2result[yIdx] = '_'
+            yIdx -= 1
+    
+    startIdx = max_len
+    for i in range(max_len,0,-1):
+        if string2result[i] == '_' and string1result[i] == '_':
+            startIdx = i + 1
+            break 
+    end_time = get_time_point()
+    end_memory = get_memory_point()
+    return "".join(string1result[startIdx:]),"".join(string2result[startIdx:]),dp[-1][-1],get_time_diff(start_time,end_time),get_memory_diff(start_memory,end_memory)
+
+
+
+#Space Optimized Efficient Version
 def SpaceEfficientAlignment(string1,string2):
     B=[[float("inf") for i in range(2)] for j in range(len(string1)+1)]
     m,n = len(string1),len(string2)
--- README.md
+++ README.md
@@ -13,3 +13,23 @@ Task: Sequence Alignment problem
 
 @Team:
 - Finalise on the documentation
+
+
+Basic Algorithm
+
+The basic or naive implementation of the Sequence Alignment Problem involves the usage of Dynamic Programming. The basic approach involves usage of a matrix of dimension m * n where m and n are the lengths of the strings s1 and s2 respectively.  The runtime complexity of the algorithm is O(mn). The recurrance equation is framed with respective to the paramters like delta and penalty for the different mismatches.
+
+
+Time Complexity - O(mn)
+Space Complexity - O(mn)
+
+Efficient Algorithm
+
+The efficient algorithm makes use of Divide and Conquer Strategy for Sequence Alignment Problem.
+
+We find the minimum cost for the operations to solve the alignment problem. We then make use of the cost to determine the split points for the strings to divide them. In this approach we become aware of the split points and divide the strings appropriately to get 2 subproblems which are repeatedly broken down to smaller subproblems and finally we make use of our basic algorithm to perform the computation.
+
+Time Complexity - O(mn)
+Space Complexity - O(2m)
+
+This is one of the most efficient solutions with respective to the space complexity.
